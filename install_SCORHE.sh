dadab2c4929bcd7d2c8bc654c1e2af7dd88231eb
noahc19 <76454253+yeric1789@users.noreply.github.com>
2023-06-22 09:09:53 -0400

    Edits install document

--- install_SCORHE.sh
+++ install_SCORHE.sh
@@ -26,7 +26,7 @@ fi
 sudo apt-get -y install python3-pyqt5 && \
 pip3 install numpy opencv-python paramiko typing matplotlib pygo pygi requests
 
-sudo apt-get install libgstreamer1.0-0 gstreamer1.0-plugins-base gstreamer1.0-plugins-good gstreamer1.0-plugins-bad gstreamer1.0-plugins-ugly gstreamer1.0-libav gstreamer1.0-doc gstreamer1.0-tools gstreamer1.0-x gstreamer1.0-alsa gstreamer1.0-gl gstreamer1.0-gtk3 gstreamer1.0-qt5 gstreamer1.0-pulseaudio
+sudo apt-get -y install libgstreamer1.0-0 gstreamer1.0-plugins-base gstreamer1.0-plugins-good gstreamer1.0-plugins-bad gstreamer1.0-plugins-ugly gstreamer1.0-libav gstreamer1.0-doc gstreamer1.0-tools gstreamer1.0-x gstreamer1.0-alsa gstreamer1.0-gl gstreamer1.0-gtk3 gstreamer1.0-qt5 gstreamer1.0-pulseaudio
 
 cd ~/Downloads/
 
@@ -36,5 +36,5 @@ sudo apt install -y ./gpac_2.2.1-rev0-gb34e3851-release-2.2_amd64.deb  # Or what
 
 cd $SCORHE_DIR
 
-echo "SCORHE dependencies installed."
+echo "\n\nSCORHE dependencies installed."
 
